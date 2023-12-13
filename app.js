f19f40614dd5c1aedca4b7e68bba37e2a6d7e930
celiasg21 <99640934+celiasg21@users.noreply.github.com>
2023-12-13 19:53:30 +0100

    cors1

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+var cors = require('cors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
