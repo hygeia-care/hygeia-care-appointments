0710f827509e8bba95dbf519744cf6df744adc8c
Celia Sánchez Gaitán <celia@MacBook-de-Celia.local>
2024-01-16 12:48:34 +0100

    Pruebas de Integración

--- tests/integration/dbIntegration.test.js
+++ tests/integration/dbIntegration.test.js
@@ -6,90 +6,68 @@ const app = require('../../app');
 jest.setTimeout(3000);
 
 describe("Integration Tests", () => {
-    beforeAll(async () => {
-        await dbConnectTest.connectToTestDatabase();
+
+    beforeAll((done) => {
+        if (dbConnectTest.readyState == 1) {
+            done();
+        } else {
+            dbConnectTest.on("connected", () => done());
+        }
     });
-    
 
-    describe("Integration tests", () => {
-        const appointments = [
-            new Appointment({ 
-                nameDoctor: 'Doctor1',
-                lastnameDoctor: 'ApellidoDoctor1',
-                idPatient: '123',
-                namePatient: 'Paciente1',
-                lastnamePatient: 'ApellidoPaciente1',
-                date: new Date('2024-01-15T09:00:00.000Z'),
-                subject: 'Asunto1'
-            }),
-        ];
+    describe("Appointments DB connection", () => {
+        const appointmentData = {
+            nameDoctor: 'DoctorName',
+            lastnameDoctor: 'DoctorLastName',
+            idPatient: '123',
+            namePatient: 'PatientName',
+            lastnamePatient: 'PatientLastName',
+            date: new Date(),
+            subject: 'AppointmentSubject'
+        };
 
-        var dbFind;
-        var dbDeleteOne;
+        var result;
 
         beforeAll(async () => {
             await Appointment.deleteMany({});
-            await Appointment.insertMany(appointments);
         });
 
-        it("Fetches appointments from the DB", async () => {
-            dbFind = jest.spyOn(Appointment, 'find');
-            const response = await request(app).get("/api/v1/appointments");
-            expect(response.statusCode).toBe(200);
-            expect(response.body).toHaveLength(appointments.length);
-            expect(dbFind).toBeCalled();
+        it("Writes an appointment in the DB", async () => {
+            const appointment = new Appointment(appointmentData);
+            await appointment.save();
+            result = await Appointment.find();
+            expect(result).toHaveLength(1);
         });
 
-        it("Deletes appointment by date and patient ID", async () => {
-            const appointment = appointments[0]; // Obtén una cita de la base de datos o crea una para la prueba
-        
-            // Realiza la eliminación
-            const response = await request(app).delete(`/api/v1/appointments/date/${appointment.date.toISOString()}/patient/${appointment.idPatient}`);
-            
-            // Verifica que la respuesta sea exitosa (código 200) si la cita se elimina correctamente
-            if (response.statusCode === 200) {
-                // Intenta recuperar el elemento eliminado de la base de datos
-                const deletedAppointment = await Appointment.findById(appointment._id);
-        
-                // Verifica que el elemento no exista en la base de datos
-                expect(deletedAppointment).toBeNull();
-            } else {
-                // Verifica que la respuesta sea 404 si la cita no se encuentra
-                expect(response.statusCode).toBe(404);
-                expect(response.body.error).toEqual("Appointment not found for the given date and patient ID");
-            }
+        it("Reads appointment from the DB", async () => {
+            const appointment = new Appointment(appointmentData);
+            await appointment.save();
+            const savedAppointment = await Appointment.findById(appointment._id);
+            expect(savedAppointment.nameDoctor).toEqual('DoctorName');
         });
 
+        it("Deletes appointment from the DB", async () => {
+            const appointment = new Appointment(appointmentData);
+            await appointment.save();
         
+            // Simular la eliminación utilizando la ruta específica
+            const deleteResponse = await request(app)
+                .delete(`/api/v1/appointments/date/${appointment.date.toISOString()}/patient/${appointment.idPatient}`);
         
-
-        it("Returns 404 if the appointment does not exist", async () => {
-            const response = await request(app).delete("/api/v1/appointments/nonexistentid");
-        
-            // Verifica que la respuesta sea 404
-            expect(response.statusCode).toBe(404);
-        
-            // Verifica que el cuerpo de la respuesta sea un objeto (puede ser un objeto vacío)
-            expect(response.body).toBeInstanceOf(Object);
-        
-            // Verifica que la propiedad 'error' exista en el cuerpo de la respuesta
-            if ('error' in response.body) {
-                // Verifica que el valor de la propiedad 'error' sea igual a "Appointment not found"
-                expect(response.body.error).toEqual("Appointment not found");
-            } else {
-                // Puedes manejar la situación en la que 'error' no está presente según tus necesidades
-                // Por ejemplo, asumir que la respuesta es válida aunque no haya 'error'
-            }
+            expect(deleteResponse.statusCode).toBe(200);
         });
         
-
-        afterEach(async () => {
-            await Appointment.deleteMany({});
-        });
+        
 
         afterAll(async () => {
-            await dbConnectTest.closeTestDatabase();
+            await Appointment.deleteMany({});
         });
-          
     });
+
+    afterAll(async () => {
+        if (dbConnectTest.readyState == 1) {
+            await dbConnectTest.close();
+        }
+    });
+
 });
--- tests/integration/envDBIntegrationTest.js
+++ tests/integration/envDBIntegrationTest.js
@@ -1,26 +1,13 @@
 const mongoose = require('mongoose');
 
-const DB_URL_TEST = 'mongodb+srv://assurance_db_user:Q1zgoIFMyIEjAoAs@cluster0.miuwv1w.mongodb.net/bills_db_test?retryWrites=true&w=majority';
+// Database for integration tests
+const DB_URL_TEST = (process.env.DB_URL_TEST || 'mongodb+srv://celia_Appointment:celia_Appointment@cluster0.miuwv1w.mongodb.net/appointments');
+console.log("Connecting to database: %s", DB_URL_TEST);
 
-const connectToTestDatabase = async () => {
-    try {
-        if (mongoose.connection.readyState !== 0) {
-            await mongoose.connection.close();
-        }
-        await mongoose.connect(DB_URL_TEST, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log("Connected to the test database:", DB_URL_TEST);
-    } catch (error) {
-        console.error("Error connecting to the test database:", error.message);
-        throw error;
-    }
-};
+mongoose.connect(DB_URL_TEST);
+const dbConnectTest = mongoose.connection;
 
-const closeTestDatabase = async () => {
-    await mongoose.connection.close();
-    console.log("Closed connection to the test database");
-};
+dbConnectTest.on('error', console.error.bind(console, 'dbConnectTest connection error'));
 
-module.exports = {
-    connectToTestDatabase,
-    closeTestDatabase
-};
+// Export
+module.exports = dbConnectTest;
